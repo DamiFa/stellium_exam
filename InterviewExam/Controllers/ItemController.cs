f8437a955f9fdcc96fb1d9df6c78e4530ea916c8
DFA <fougi2@gmail.com>
2022-08-28 11:28:33 +0200

    add document architecture + fixing steps + clean up

--- InterviewExam/Controllers/ItemController.cs
+++ InterviewExam/Controllers/ItemController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InterviewExam.Controllers
 {
-    //[ApiController]
     [Route("api/[controller]/[action]")]
     public class ItemController : ControllerBase
     {
